ff824583915c7c867495b30fcc3b1a758182cc36
alfa04 <joao.carlos.costa@fe.up.pt>
2015-12-27 19:40:04 +0000

    Server [incompleto ainda]

--- reader/plgame/hecatomb.pl
+++ reader/plgame/hecatomb.pl
@@ -73,7 +73,7 @@ gameMode(_):-write('Wrong option, choose a new one!'), nl, read(Choice), gameMod
 play(pvp):-
     random(1,3, Player), write('Player number '), 
     write(Player), write(' will start the game!'),nl,
-    board(B),printB(B),pvpgame(Player, B, 10).
+    board(B),printB(B),pvpgame(Player, B, 10,4,3,4,4).
 
 play(hbf):-random(1,3, Player), 
     ((Player == 1)->write('Human starts playing!'); 
@@ -92,19 +92,19 @@ play(bvb):-random(1,3, Player),write('Player number '),
     board(B),printB(B),bvbgame(Player, B, 10).
 
 %Checks if the game is over
-pvpgame(_,Board,_):-            
+pvpgame(_,Board,_,_,_,_,_,_):-            
         game_over(Board).
 
-pvpgame(_,Board,0):-gameDraw(Board).
+pvpgame(_,Board,0,_,_,_,_,_):-gameDraw(Board).
 
-pvpgame(P,Board,Turns):-
+pvpgame(P,Board,Turns,Xi,Yi,Xf,Yf,B3):-
     Turns > 0, 
     Turns2 is Turns-1,
     printPlayer(P,h), 
     movement(Xi,Yi,Xf,Yf,P,Board),
     getPiece(Xi,Yi,Board, Element),
     replace(Board, Xf, Yf, Element, B2), 
-    replace(B2, Xi, Yi, '   ', B3), printB(B3),changePlayer(P,Novo),pvpgame(Novo,B3,Turns2).
+    replace(B2, Xi, Yi, ' ', B3), printB(B3),changePlayer(P,Novo),!.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 pvbgame(_,Board,_,_):-            
@@ -122,7 +122,7 @@ pvbgame(P,Board,Turns,Dif):-
     write(Xi-Yi-Xf-Yf),
     getPiece(Xi,Yi,Board,Element),
     replace(Board, Xf, Yf, Element, B2), 
-    replace(B2, Xi, Yi, '   ', B3), printB(B3),changePlayer(P,Novo), pvbgame(Novo,B3,Turns2,Dif).
+    replace(B2, Xi, Yi, ' ', B3), printB(B3),changePlayer(P,Novo), pvbgame(Novo,B3,Turns2,Dif).
 
 bvbgame(_,Board,_):-            
         game_over(Board).
@@ -137,7 +137,7 @@ bvbgame(P,Board,Turns):-
     write(Xi-Yi-Xf-Yf),
     getPiece(Xi,Yi,Board,Element),
     replace(Board, Xf, Yf, Element, B2), 
-    replace(B2, Xi, Yi, '   ', B3), printB(B3),changePlayer(P,Novo), bvbgame(Novo,B3,Turns2).
+    replace(B2, Xi, Yi, ' ', B3), printB(B3),changePlayer(P,Novo), bvbgame(Novo,B3,Turns2).
      
 replace( L , Y , X , Z , R ) :-
   append(RowPfx,[Row|RowSfx],L),     % decompose the list-of-lists into a prefix, a list and a suffix
@@ -168,35 +168,35 @@ positionsDifferent(Xi,Yi,Xf,Yf) :-
         (Xi \= Xf; Yi \= Yf).
 
 checkCorrectPiece(1,Board,Xi,Yi,Xf,Yf):-getPiece(Xi,Yi,Board,InP), getPiece(Xf,Yf,Board,FnP),
-    ((InP == ' $ '; InP == ' + '),(FnP == ' & '; FnP == ' * '; FnP == '   '));
+    ((InP == '$'; InP == '+'),(FnP == '&'; FnP == '*'; FnP == ' '));
     (write('You can only play YOUR pieces and eat your oponent´s!'),nl,fail).
     
 checkCorrectPiece(2,Board,Xi,Yi,Xf,Yf):-getPiece(Xi,Yi,Board,InP), getPiece(Xf,Yf,Board,FnP),
-    ((InP == ' & '; InP == ' * '),(FnP == ' $ '; FnP == ' + '; FnP == '   '));
+    ((InP == '&'; InP == '*'),(FnP == '$'; FnP == '+'; FnP == ' '));
     (write('You can only play YOUR pieces and eat your oponent´s!'),nl,fail).
     
 checkMovement(Xi,Yi,Xf,Yf,Board):-
     getPiece(Xi,Yi,Board,P), checkPlay(Xi,Yi,Xf,Yf,P,D), checkPath(Xi,Yi,Xf,Yf,P,D,Board,0).
     
-checkPlay(Xi,Yi,Xf,Yf,' $ ',D):-
+checkPlay(Xi,Yi,Xf,Yf,'$',D):-
     (Xi == Xf, Yi \= Yf)->(D = 'V');
     (Xi \= Xf, Yi == Yf)->(D = 'H');
     (abs(Xi - Xf) =:= abs(Yi - Yf))->(D = 'D');
     (write('Soldiers can only move orthogonaly or diagonaly!'),nl,fail).
     
-checkPlay(Xi,Yi,Xf,Yf,' & ',D):-
+checkPlay(Xi,Yi,Xf,Yf,'&',D):-
     (Xi == Xf, Yi \= Yf)->(D = 'V');
     (Xi \= Xf, Yi == Yf)->(D = 'H');
     (abs(Xi - Xf) =:= abs(Yi - Yf))->(D = 'D');
     (write('Soldiers can only move orthogonaly or diagonaly!'),nl,fail).
     
-checkPlay(Xi,Yi,Xf,Yf,' + ',_):-
+checkPlay(Xi,Yi,Xf,Yf,'+',_):-
     ((Xi == Xf, (Yi == (Yf-1); Yi == (Yf+1)));
     (((Xi == (Xf-1); Xi == Xf+1)), Yi == Yf);
     ((abs(Xi - Xf) =:= abs(Yi - Yf)),abs(Xi - Xf) =:= 1));
     (write('Kings can only move orthogonaly or diagonaly to and adjacent cell!'),nl,fail).
     
-checkPlay(Xi,Yi,Xf,Yf,' * ',_):-
+checkPlay(Xi,Yi,Xf,Yf,'*',_):-
     ((Xi == Xf, (Yi == (Yf-1); Yi == (Yf+1)));
     (((Xi == (Xf-1); Xi == Xf+1)), Yi == Yf);
     ((abs(Xi - Xf) =:= abs(Yi - Yf)),abs(Xi - Xf) =:= 1));
@@ -208,14 +208,14 @@ checkPath(Xi,Yi,Xf,Yf,P,'V',Board,N):-
     (Yi < Yf)->( 
     Y2 is Yi + 1,
     getPiece(Xi,Y2,Board,Piece),
-    ((Piece == '   ')->(N2 is N, checkPath(Xi,Y2,Xf,Yf,P,'V',Board,N));
+    ((Piece == ' ')->(N2 is N, checkPath(Xi,Y2,Xf,Yf,P,'V',Board,N));
     (Piece \= P)->(N2 is N + 1, checkPath(Xi,Y2,Xf,Yf,P,'V',Board,N2));
     fail)
     );
     (Yi > Yf)->( 
     Y2 is Yi - 1,
     getPiece(Xi,Y2,Board,Piece),
-    ((Piece == '   ')->(N2 is N, checkPath(Xi,Y2,Xf,Yf,P,'V',Board,N));
+    ((Piece == ' ')->(N2 is N, checkPath(Xi,Y2,Xf,Yf,P,'V',Board,N));
     (Piece \= P)->(N2 is N + 1, checkPath(Xi,Y2,Xf,Yf,P,'V',Board,N2));
     fail)
     );
@@ -227,14 +227,14 @@ checkPath(Xi,Yi,Xf,Yf,P,'H',Board,N):-
     (Xi < Xf)->( 
     X2 is Xi + 1,
     getPiece(X2,Yi,Board,Piece),
-    ((Piece == '   ')->(N2 is N, checkPath(X2,Yi,Xf,Yf,P,'H',Board,N));
+    ((Piece == ' ')->(N2 is N, checkPath(X2,Yi,Xf,Yf,P,'H',Board,N));
     (Piece \= P)->(N2 is N + 1, checkPath(X2,Yi,Xf,Yf,P,'H',Board,N2));
     fail)
     );
     (Xi > Xf)->( 
     X2 is Xi - 1,
     getPiece(X2,Yi,Board,Piece),
-    ((Piece == '   ')->(N2 is N, checkPath(X2,Yi,Xf,Yf,P,'H',Board,N));
+    ((Piece == ' ')->(N2 is N, checkPath(X2,Yi,Xf,Yf,P,'H',Board,N));
     (Piece \= P)->(N2 is N + 1, checkPath(X2,Yi,Xf,Yf,P,'H',Board,N2));
     fail)
     );
@@ -248,7 +248,7 @@ checkPath(Xi,Yi,Xf,Yf,P,'D',Board,N):-
     Y2 is Yi + 1,
     X2 is Xi + 1,
     getPiece(X2,Y2,Board,Piece),
-    ((Piece == '   ')->(N2 is N, checkPath(X2,Y2,Xf,Yf,P,'D',Board,N));
+    ((Piece == ' ')->(N2 is N, checkPath(X2,Y2,Xf,Yf,P,'D',Board,N));
     (Piece \= P)->(N2 is N + 1, checkPath(X2,Y2,Xf,Yf,P,'D',Board,N2));
     fail)
     );
@@ -256,7 +256,7 @@ checkPath(Xi,Yi,Xf,Yf,P,'D',Board,N):-
     Y2 is Yi - 1,
     X2 is Xi - 1,
     getPiece(X2,Y2,Board,Piece),
-    ((Piece == '   ')->(N2 is N, checkPath(X2,Y2,Xf,Yf,P,'D',Board,N));
+    ((Piece == ' ')->(N2 is N, checkPath(X2,Y2,Xf,Yf,P,'D',Board,N));
     (Piece \= P)->(N2 is N + 1, checkPath(X2,Y2,Xf,Yf,P,'D',Board,N2));
     fail)
     );
@@ -264,7 +264,7 @@ checkPath(Xi,Yi,Xf,Yf,P,'D',Board,N):-
     Y2 is Yi - 1,
     X2 is Xi + 1,
     getPiece(X2,Y2,Board,Piece),
-    ((Piece == '   ')->(N2 is N, checkPath(X2,Y2,Xf,Yf,P,'D',Board,N));
+    ((Piece == ' ')->(N2 is N, checkPath(X2,Y2,Xf,Yf,P,'D',Board,N));
     (Piece \= P)->(N2 is N + 1, checkPath(X2,Y2,Xf,Yf,P,'D',Board,N2));
     fail)
     );
@@ -272,7 +272,7 @@ checkPath(Xi,Yi,Xf,Yf,P,'D',Board,N):-
     Y2 is Yi + 1,
     X2 is Xi - 1,
     getPiece(X2,Y2,Board,Piece),
-    ((Piece == '   ')->(N2 is N, checkPath(X2,Y2,Xf,Yf,P,'D',Board,N));
+    ((Piece == ' ')->(N2 is N, checkPath(X2,Y2,Xf,Yf,P,'D',Board,N));
     (Piece \= P)->(N2 is N + 1, checkPath(X2,Y2,Xf,Yf,P,'D',Board,N2));
     fail)
     );
@@ -280,7 +280,6 @@ checkPath(Xi,Yi,Xf,Yf,P,'D',Board,N):-
 
 movePiece(Xi,Yi,Xf,Yf,Player,Board):- 
          repeat,
-         getPlayerMovement(Xi,Yi,Xf,Yf),
          positionsDifferent(Xi,Yi,Xf,Yf),
          checkOutOfBounds(Xi,Yi,Xf,Yf),
          checkCorrectPiece(Player,Board,Xi,Yi,Xf,Yf),
@@ -302,11 +301,11 @@ testMoves(Board,Player,Value,XIndex,YIndex,XFIndex,YFIndex):-
                                                                                 
 allPossibleMoves(Board,Player,Value,X,Y,XF,YF):- 
     ((Player == 1),
-    (getPiece(XIndex,YIndex,Board,' & '); getPiece(XIndex,YIndex,Board,' * ')),
-    (getPiece(XFIndex,YFIndex,Board,' $ ');getPiece(XIndex,YIndex,Board,' + ')));
+    (getPiece(XIndex,YIndex,Board,'&'); getPiece(XIndex,YIndex,Board,'*')),
+    (getPiece(XFIndex,YFIndex,Board,'$');getPiece(XIndex,YIndex,Board,'+')));
     ((Player == 2),
-    (getPiece(XIndex,YIndex,Board,' $ '); getPiece(XIndex,YIndex,Board,' + ')),
-    (getPiece(XFIndex,YFIndex,Board,' & ');getPiece(XIndex,YIndex,Board,' * '))),
+    (getPiece(XIndex,YIndex,Board,'$'); getPiece(XIndex,YIndex,Board,'+')),
+    (getPiece(XFIndex,YFIndex,Board,'&');getPiece(XIndex,YIndex,Board,'*'))),
     testMoves(Board,Player,Value,XIndex,YIndex,XFIndex,YFIndex),
     X is XIndex,Y is YIndex, XF is XFIndex, YF is YFIndex.
 
@@ -329,8 +328,8 @@ maxList([Xi-Yi-Xf-Yf-V|T], CXi-CYi-CXf-CYf-Cv, Max) :-
         ; maxList(T, CXi-CYi-CXf-CYf-Cv, Max) ).
 
 checkValue(Board,Player,Xi,Yi,V):-
-    (Player == 1 )->getPiece(Xr,Yr,Board,' * ');
-    getPiece(Xr,Yr,Board,' + '),
+    (Player == 1 )->getPiece(Xr,Yr,Board,'*');
+    getPiece(Xr,Yr,Board,'+'),
     Xv is abs(Xr - Xi),
     Yv is abs(Yr - Yi),
     V is Xv + Yv.
@@ -358,12 +357,12 @@ gameDraw(Board):-printB(Board), write('The Game ended in a draw (reached 10 turn
 
 %Checks if player 1 has won
 game_over(Board):-          
-        \+ (getPiece(_,_,Board,' + ')),
+        \+ (getPiece(_,_,Board,'+')),
         write('Game has ended! Player 2 wins!'), nl.
 
 %Checks if player 2 has won        
 game_over(Board):-           
-        \+ (getPiece(_,_,Board,' * ')),
+        \+ (getPiece(_,_,Board,'*')),
         write('Game has ended! Player 1 wins!'), nl.
 
 %%%%%%%%%%FUNCAO DE TESTE PARA SERVIDOR, test(C,N)
--- reader/plgame/server.pl
+++ reader/plgame/server.pl
@@ -107,7 +107,8 @@ print_header_line(_).
 
 parse_input(handshake, handshake).
 parse_input(test(C,N), Res) :- test(C,Res,N).
-parse_input(play(X), board(B)) :- play(X).
+parse_input(play(X), done) :- play(X).
+parse_input(pvpgame(P,Board,Turns,Xi,Yi,Xf,Yf), [P,B3]) :- pvpgame(P,Board,Turns,Xi,Yi,Xf,Yf,B3).
 parse_input(quit, goodbye).
-
-	
+%pvpgame(1,[['$','$','$','$','+','$','$','$'],['$','$','$','$','$','$','$','$'],['$','$','$','$','$','$','$','$'],['$','$','$','$','$','$','$','$'],['&','&','&','&','&','&','&','&'],['&','&','&','&','&','&','&','&'],['&','&','&','&','&','&','&','&'],['&','&','&','&','*','&','&','&']],10,4,3,4,4)
+%play(pvp)
